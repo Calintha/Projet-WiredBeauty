7c444d8e7273591f17aeae5141a8d54cd24cd964
Alexmdz77 <alext77165@gmail.com>
2022-03-10 09:52:13 +0100

    add wordpress & custom theme

---
+++ wordpress/wp-content/themes/wiredbeauty/page.php
@@ -0,0 +1,21 @@
+<?php
+
+get_header();
+
+?>
+
+<div class="container container-page">
+    <div class="content">
+
+    <?php
+
+    the_content();
+
+    ?>
+
+    </div>
+</div>
+
+<?php
+wp_footer();
+get_footer();
